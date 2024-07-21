9d0ddc2e9ac4af7ba790da07c701e09d50be0184
TreSet <hanneshekl@gmail.com>
2024-07-21 10:58:32 +0200

    Added Basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import {ThemeToggle} from "@/app/ui/theme-toggle";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Portal Linker</h1>
+        <ThemeToggle></ThemeToggle>
     </main>
   );
 }
